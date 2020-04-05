026a6b64f3e59ae94b11207195a772c390fe3d2b
LucLop <lo.lucas@outlook.com>
2020-04-05 19:27:54 +0200

    Creating API project and controllers for bagage and vol models

---
+++ MyAiport.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MyAiport.API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ MyAiport.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyAiport.API.WebApiApplication" Language="C#" %>
