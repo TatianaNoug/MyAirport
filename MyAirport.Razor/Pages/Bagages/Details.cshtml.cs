59ee6901df7d6519ed250f96d817552a77ad61ff
TatianaNoug <tatianameister.tm@gmail.com>
2020-04-15 19:27:59 +0200

    razor pages automatically generated

---
+++ MyAirport.Razor/Pages/Bagages/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using LLTM.MyAirport.EF;
+
+namespace MyAirport.Razor.Pages.Bagages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly LLTM.MyAirport.EF.MyAirportContext _context;
+
+        public DetailsModel(LLTM.MyAirport.EF.MyAirportContext context)
+        {
+            _context = context;
+        }
+
+        public Bagage Bagage { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Bagage = await _context.Bagages.FirstOrDefaultAsync(m => m.BagageID == id);
+
+            if (Bagage == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ MyAirport.Razor/Pages/Vols/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using LLTM.MyAirport.EF;
+
+namespace MyAirport.Razor.Pages.Vols
+{
+    public class IndexModel : PageModel
+    {
+        private readonly LLTM.MyAirport.EF.MyAirportContext _context;
+
+        public IndexModel(LLTM.MyAirport.EF.MyAirportContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Vol> Vol { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Vol = await _context.Vols.ToListAsync();
+        }
+    }
+}
