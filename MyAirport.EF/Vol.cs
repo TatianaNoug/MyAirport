6fef95598cf431d81e46a24082899522a68dbc6b
Lucas Lopez <44178424+LucLop@users.noreply.github.com>
2020-04-18 16:17:03 +0200

    Update Vol.cs

--- MyAirport.EF/Vol.cs
+++ MyAirport.EF/Vol.cs
@@ -7,6 +7,9 @@ namespace LLTM.MyAirport.EF
 {
     public class Vol
     {
+        /// <summary>
+        /// Vol Constructor
+        /// </summary>
         public Vol()
         {
             Cie = "";
@@ -14,33 +17,61 @@ namespace LLTM.MyAirport.EF
             Bagages = new List<Bagage>();
         }
 
+        /// <summary>
+        /// Vol Key
+        /// </summary>
         [Key]
         public int VolID { get; set; }
 
+        
+        /// <summary>
+        /// Vol Company
+        /// </summary>
         [Column(TypeName = "char(10)")]
         [Required]
         [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         public string Cie { get; set; }
 
+        /// <summary>
+        /// Vol Lig
+        /// </summary>
         [Column(TypeName = "char(5)")]
         public string Lig { get; set; }
 
+        /// <summary>
+        /// Vol Dhc
+        /// </summary>
         public DateTime Dhc { get; set; }
 
+        /// <summary>
+        /// Vol Pkg
+        /// </summary>
         [Column(TypeName = "char(3)")]
         public string? Pkg { get; set; }
 
+        /// <summary>
+        /// Vol Imm
+        /// </summary>
         [Column(TypeName = "char(6)")]
         public string? Imm { get; set; }
 
+        /// <summary>
+        /// Vol Pax
+        /// </summary>
         public short? Pax { get; set; }
 
+        /// <summary>
+        /// Vol Destination
+        /// </summary>
         [Column(TypeName = "char(3)")]
         [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
         [Required]
         public string? Des { get; set; }
 
 
+        /// <summary>
+        /// Bagages flying with this Vol
+        /// </summary>
         public virtual ICollection<Bagage> Bagages { get; set; }
     }
 }
